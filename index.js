2717c115677d3728ca792a8a1ed75c44e7093244
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-04-16 19:08:05 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,18 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  const hour = parseInt(timeString.split(':')[0]);
+  if (hour < 12) {
+    return "Good Morning";
+  } else if (hour < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  const element = document.getElementById("greeting");
+  element.innerText = message;
+}
